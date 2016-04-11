3e09d67f1dd58fd7b43e67ff52a3528e946e1bf6
Toomas Lõõnik <toomloo@greeny.cs.tlu.ee>
2016-04-11 11:35:21 +0300

    mingi viga sees kuskil

---
+++ script.js
@@ -0,0 +1,72 @@
+var $grid;
+
+$(function() {
+
+
+    console.log("leht on laetud");
+
+    getTweets();
+
+    $grid = $('#feed').isotope({
+      itemSelector: '.item'
+    });
+
+
+});
+
+var getTweets = function(){
+  console.log('getting tweets...');
+
+  $.ajax({
+    url: "getFeed.php",
+    success: function(result){
+      //console.log(JSON.parse(result).api.statuses);
+
+      printTweets(JSON.parse(result).api.statuses);
+
+    },
+    error: function(xhr,status,error){
+      console.log(error);
+    }
+  });
+
+};
+
+var printTweets = function(ajaxStatuses){
+
+  var items = '';
+
+  //foreach
+  $.each(ajaxStatuses, function(index, tweet){
+
+    /*
+      <div class="item">
+        <img class="profile-img" src="" alt="">
+        <div class="author"></div>
+        <p class="text"></p>
+      </div>
+    */
+
+    items +=  '<div class="item">'+
+                  '<div class="profile-img" style="background-image: url('+tweet.user.profile_image_url.replace("_normal","")+')" ></div>'+
+                  '<div class="author">'+tweet.user.name+'</div>'+
+                  '<p class="text">'+tweet.text+'</p>'+
+                '</div>';
+
+    //console.log($(item));
+
+  });
+
+  //kÃµik korraga
+  var temp = $(items);
+
+  $grid.prepend(temp)
+       .isotope( 'prepended', temp )
+       .isotope('layout');
+
+  //get new every 5 seconds
+  window.setTimeout(function(){
+    getTweets();
+  }, 5000);
+
+};
